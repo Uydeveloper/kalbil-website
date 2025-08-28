59e38fa989ea602b23419d6892e2925f4fbd588a
Kawuli Abudureyimu <145730569+KawulJanSetouchi@users.noreply.github.com>
2025-08-28 11:37:25 +0900

    Update some section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,7 +71,7 @@ export default function Footer() {
 
           <div className="mt-4">
             <a
-              href="/"
+              href="https://uydeveloper.github.io/kalbil-website/#/about"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700 transition text-sm"
